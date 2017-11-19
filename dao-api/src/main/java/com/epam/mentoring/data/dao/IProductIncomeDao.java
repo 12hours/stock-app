2d0d696eb6c069caf86cf73bec7dce9ed7b4708d
nm <mon-keigh@ya.ru>
2017-11-19 13:59:32 +0000

    dao-api updated

--- dao-api/src/main/java/com/epam/mentoring/data/dao/IProductIncomeDao.java
+++ dao-api/src/main/java/com/epam/mentoring/data/dao/IProductIncomeDao.java
@@ -4,7 +4,50 @@ import org.springframework.dao.DataAccessException;
 
 import com.epam.mentoring.data.model.ProductIncome;
 
+import java.util.List;
+
+/**
+ * Interface for ProductIncome DAO
+ */
 public interface IProductIncomeDao {
+
+	/**
+	 * Finds ProductIncome record with specified id
+	 * @param id target ProductIncome's id
+	 * @return ProductIncome
+	 * @throws DataAccessException
+	 */
 	ProductIncome getProductIncomeById(Integer id) throws DataAccessException;
+
+	/**
+	 * Saves provided ProductIncome object into database
+	 * @param productIncome
+	 * @return not specified
+	 * @throws DataAccessException
+	 */
 	Integer addProductIncome(ProductIncome productIncome) throws DataAccessException;
+
+	/**
+	 * Deletest ProductIncome record with specified id from database
+	 * @param id target ProductIncome's id
+	 * @return not specified
+	 * @throws DataAccessException
+	 */
+	Integer deleteProductIncome(Integer id) throws DataAccessException;
+
+	/**
+	 * Updates ProductIncome record;
+	 * ProductIncome is identified by id field;
+	 * @param productIncome updated object
+	 * @return not specified
+	 * @throws DataAccessException
+	 */
+	Integer updateProductIncome(ProductIncome productIncome) throws DataAccessException;
+
+	/**
+	 * Finds all ProductIncome records in database
+	 * @return List of ProductIncome objects
+	 * @throws DataAccessException
+	 */
+	List<ProductIncome> getAllProductIncomes() throws DataAccessException;
 }
--- dao-api/src/main/java/com/epam/mentoring/data/dao/ISupplierDao.java
+++ dao-api/src/main/java/com/epam/mentoring/data/dao/ISupplierDao.java
@@ -6,10 +6,49 @@ import org.springframework.dao.DataAccessException;
 
 import com.epam.mentoring.data.model.Supplier;
 
+/**
+ * Interface for {@code Supplier} DAO;
+ * Describes basic CRUD operations
+ */
 public interface ISupplierDao {
+
+	/**
+	 * Finds all {@code Supplier} records in database
+	 * @return list of suppliers
+	 * @throws DataAccessException
+	 */
 	List<Supplier> getAllSuppliers() throws DataAccessException;
+
+	/**
+	 * Saves provided {@code Supplier} object to database
+	 * @param supplier Supplier to save
+	 * @return not specified
+	 * @throws DataAccessException
+	 */
 	int addSupplier(Supplier supplier) throws DataAccessException;
+
+	/**
+	 * Updates {@code Supplier} record;
+	 * Supplier record is identified by id field of provided {@code Supplier} object
+	 * @param supplier updated Supplier
+	 * @return not specified
+	 * @throws DataAccessException
+	 */
 	int updateSupplier(Supplier supplier) throws DataAccessException;
+
+	/**
+	 * Deletes {@code Supplier} record with specified id from database
+	 * @param id id of record to delete
+	 * @return not specified
+	 * @throws DataAccessException
+	 */
 	int deleteSupplier(Integer id) throws DataAccessException;
+
+	/**
+	 * Finds {@code Supplier} record with specified id
+	 * @param id target supplier id
+	 * @return Supplier
+	 * @throws DataAccessException
+	 */
 	Supplier getSupplierById(Integer id) throws DataAccessException;
 }
