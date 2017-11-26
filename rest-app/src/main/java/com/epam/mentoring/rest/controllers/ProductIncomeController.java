2f3917635a32c184ba3d0f337f7a7687f2076fcd
nm <mon-keigh@ya.ru>
2017-11-26 14:53:49 +0000

    ProductIncomeController: logger added

--- rest-app/src/main/java/com/epam/mentoring/rest/controllers/ProductIncomeController.java
+++ rest-app/src/main/java/com/epam/mentoring/rest/controllers/ProductIncomeController.java
@@ -3,16 +3,15 @@ package com.epam.mentoring.rest.controllers;
 import com.epam.mentoring.data.model.dto.ProductIncomeForm;
 import com.epam.mentoring.rest.error.CannotSaveException;
 import com.epam.mentoring.service.IProductIncomeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -21,6 +20,8 @@ import javax.validation.Valid;
 @RestController
 //@RequestMapping("/income")
 public class ProductIncomeController {
+
+    Logger logger = LoggerFactory.getLogger(ProductIncomeController.class.getName());
 
     private IProductIncomeService productIncomeService;
 
@@ -32,12 +33,14 @@ public class ProductIncomeController {
     public ResponseEntity<Void> addIncome(@Valid @RequestBody ProductIncomeForm productIncomeForm,
                                           UriComponentsBuilder ucBuilder) throws CannotSaveException {
         try {
+            logger.debug("trying to add new income: " + productIncomeForm.toString());
             Integer id = productIncomeService.saveProductIncome(productIncomeForm);
             HttpHeaders headers = new HttpHeaders();
 //            headers.setLocation(ucBuilder.path("/income/{id}").buildAndExpand(id).toUri());
             headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
             return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
         } catch (DataAccessException ex) {
+            logger.debug("can not add new income: " + ex.getMessage());
             throw new CannotSaveException();
         }
     }
