74339a6c9815a19f9f1c0bc9d79095a5d02899a2
nm <mon-keigh@ya.ru>
2017-11-26 12:42:39 +0000

    IProductConsumer updated

--- client-api/src/main/java/com/epam/mentoring/client/IProductConsumer.java
+++ client-api/src/main/java/com/epam/mentoring/client/IProductConsumer.java
@@ -7,25 +7,31 @@ import java.util.Map;
 import com.epam.mentoring.client.exception.ServerDataAccessException;
 import com.epam.mentoring.data.model.Product;
 import com.epam.mentoring.data.model.ProductType;
+import com.epam.mentoring.data.model.dto.ProductForm;
+import com.epam.mentoring.data.model.dto.ProductWithQuantityView;
 
 public interface IProductConsumer {
 	
-	public Product findProductById(Long id) throws ServerDataAccessException;
+	Product findProductById(Long id) throws ServerDataAccessException;
 	
-	public Product findProductByName(String name) throws ServerDataAccessException;
+	Product findProductByName(String name) throws ServerDataAccessException;
 	
-	public List<Product> findAllProductsByType(ProductType type) throws ServerDataAccessException;
+	List<Product> findAllProductsByType(ProductType type) throws ServerDataAccessException;
 	
-	public Integer saveProduct(Product product) throws ServerDataAccessException;
+	Integer saveProduct(Product product) throws ServerDataAccessException;
+
+	Integer saveProduct(ProductForm productForm) throws ServerDataAccessException;
 	
-	public Product updateProduct(Long id, Product product) throws ServerDataAccessException;
+	Product updateProduct(Long id, Product product) throws ServerDataAccessException;
 	
-	public List<Product> getAllProducts() throws ServerDataAccessException;
+	List<Product> getAllProducts() throws ServerDataAccessException;
 	
-	public Integer getProductQuantity(Long id) throws ServerDataAccessException;
+	Integer getProductQuantity(Long id) throws ServerDataAccessException;
 	
-	public Map<Product, Integer> getAllProductsWithQuantites() throws ServerDataAccessException;
+	Map<Product, Integer> getAllProductsWithQuantites() throws ServerDataAccessException;
+
+	List<ProductWithQuantityView> getAllProductsWithQuantitiesViews() throws ServerDataAccessException;
 	
-	public void deleteProductById(Long id) throws ServerDataAccessException;
+	void deleteProductById(Long id) throws ServerDataAccessException;
 	
 }
