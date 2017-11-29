62487b33fdb67a1a09a9c7325f4e3ddafd2cc5e4
nm <mon-keigh@ya.ru>
2017-11-29 19:45:35 +0000

    RestProductIncomeConsumer: saveProductIncome method implemented

---
+++ client/src/test/java/com/epam/mentoring/client/rest/RestProductIncomeConsumerTest.java
@@ -0,0 +1,59 @@
+package com.epam.mentoring.client.rest;
+
+import com.epam.mentoring.data.model.dto.ProductIncomeForm;
+import com.epam.mentoring.test.TestData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withCreatedEntity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class RestProductIncomeConsumerTest {
+
+    @Spy
+    RestTemplate restTemplate;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    MockRestServiceServer mockRestServiceServer;
+
+    RestProductIncomeConsumer restProductIncomeConsumer;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockRestServiceServer = MockRestServiceServer.bindTo(restTemplate).build();
+        restProductIncomeConsumer = new RestProductIncomeConsumer(restTemplate);
+    }
+
+    @Test
+    public void saveProductIncomeTest() {
+        mockRestServiceServer.expect(requestTo("localhost:8080/income"))
+                .andRespond(withCreatedEntity(URI.create("localhost:8080/income/10")));
+        doAnswer(invocation -> {
+            URI result = (URI) invocation.callRealMethod();
+            assertEquals("localhost:8080/income/10", result.toString());
+            return result;
+        }).when(restTemplate).postForLocation(any(String.class), any(ProductIncomeForm.class));
+
+        ProductIncomeForm productIncomeForm = TestData.productIncomeForms().get(0);
+        restProductIncomeConsumer.saveProductIncome(productIncomeForm);
+        verify(restTemplate, times(1)).postForLocation(any(String.class), any(ProductIncomeForm.class));
+    }
+
+}
