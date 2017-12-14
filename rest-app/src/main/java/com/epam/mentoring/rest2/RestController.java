eef998be6c2eb9b41ad0df39e84263d3a640fcac
Victor Beliakov <mon-keigh@ya.ru>
2017-12-14 12:23:57 +0300

    migrated to spring 3; modules other than rest-app temparally excluded from projct due to compile errors; rest-app is assemled as bundle and can be deployed in Jboss Fuse; it starts simple endpoint on localhost:8081;

---
+++ rest-app/src/main/java/com/epam/mentoring/rest2/RestController.java
@@ -0,0 +1,45 @@
+package com.epam.mentoring.rest2;
+
+//import com.epam.mentoring.data.model.Product;
+//import com.epam.mentoring.service.ProductService;
+import org.apache.camel.EndpointInject;
+import org.mockito.Mockito;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+@Service
+@Path("/rest")
+@Produces(MediaType.APPLICATION_JSON)
+public class RestController {
+
+//    @EndpointInject(uri = "direct-vm:productService")
+//    ProductService productService;
+
+    public RestController() {
+//        this.productService = Mockito.mock(ProductService.class);
+//        Mockito.when(productService.getAllProducts()).thenReturn(new ArrayList<Product>(){
+//            {
+//                add(new Product(1, "SomeProduct", BigDecimal.ZERO, null));
+//            }
+//        });
+    }
+
+    @GET
+    @Path("/product")
+    public Response getProducts() throws Exception{
+        Class.forName(EndpointInject.class.getName());
+        return Response.status(Response.Status.OK)
+                        .type(MediaType.APPLICATION_JSON_TYPE)
+//                        .entity(productService.getAllProducts())
+                        .entity(new String("Hello world"))
+                        .build();
+    }
+
+}
---
+++ service/src/main/java/com/epam/mentoring/service/routes/RouteNames.java
@@ -0,0 +1,9 @@
+package com.epam.mentoring.service.routes;
+
+public class RouteNames {
+
+    public static final String MAIN_SERVICE_ROUTE = "direct-vm:mainService";
+
+    public static final String MAIN_SERVICE_ID = "mainService";
+
+}
