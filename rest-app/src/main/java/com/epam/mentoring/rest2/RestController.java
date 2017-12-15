47994896f813d0baac968fac6ae5de85562fad5a
Victor Beliakov <mon-keigh@ya.ru>
2017-12-15 12:06:51 +0300

    service bundle successfully imported into rest-app

--- rest-app/src/main/java/com/epam/mentoring/rest2/RestController.java
+++ rest-app/src/main/java/com/epam/mentoring/rest2/RestController.java
@@ -2,11 +2,13 @@ package com.epam.mentoring.rest2;
 
 //import com.epam.mentoring.data.model.Product;
 //import com.epam.mentoring.service.ProductService;
+
 import com.epam.mentoring.service.ProductService;
 import org.apache.camel.Endpoint;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.*;
@@ -23,16 +25,26 @@ public class RestController {
     @EndpointInject(uri = "direct-vm:mainRoute")
     private ProducerTemplate mainRoute;
 
+    ProductService productService;
+
+    @Autowired
+    public RestController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/product")
-    public Response getProducts(String body) throws Exception{
+    public Response getProducts(String body) throws Exception {
+        System.out.println(
+                "Services! " + productService.getAllProducts()
+        );
         String answer = (String) mainRoute.requestBody(body);
         return Response.status(Response.Status.OK)
-                        .type(MediaType.APPLICATION_JSON_TYPE)
+                .type(MediaType.APPLICATION_JSON_TYPE)
 //                        .entity(productService.getAllProducts())
-                        .entity("Hello world" + answer)
-                        .build();
+                .entity("Hello world" + answer)
+                .build();
     }
 
 }
