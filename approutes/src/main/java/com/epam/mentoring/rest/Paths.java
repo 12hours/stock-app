9cf7acbaa2a1164f5f926a0fe2a2a810c7aea6ec
Victor Beliakov <mon-keigh@ya.ru>
2017-12-27 16:23:40 +0300

    product income controller works over cxf

--- approutes/src/main/java/com/epam/mentoring/rest/Paths.java
+++ approutes/src/main/java/com/epam/mentoring/rest/Paths.java
@@ -2,7 +2,7 @@ package com.epam.mentoring.rest;
 
 public class Paths {
     public static final String PRODUCT_URI = "/";
-    public static final String PRODUCT_INCOME_URI = "/rest/product_income";
+    public static final String PRODUCT_INCOME_URI = "/";
     public static final String PRODUCT_TYPE_URI = "/";
     public static final String STOCK_URI = "/rest/stock";
     public static final String SUPPLIER_URI = "/rest/supplier";
