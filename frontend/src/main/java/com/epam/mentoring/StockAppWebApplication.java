5aa152842b73a6d3b79db0a46a73bd6d4c530892
Victor Beliakov <mon-keigh@ya.ru>
2018-01-05 14:38:14 +0300

    frontend module created

---
+++ frontend/src/main/java/com/epam/mentoring/StockAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.epam.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class StockAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAppWebApplication.class, args);
+	}
+}
