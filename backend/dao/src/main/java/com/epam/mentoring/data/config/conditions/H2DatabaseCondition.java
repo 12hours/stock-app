992de4cca82d80e45d41f7a7cc29886643c5e255
Victor Beliakov <mon-keigh@ya.ru>
2018-01-09 12:34:40 +0300

    redundand modules deleted

--- backend/dao/src/main/java/com/epam/mentoring/data/config/conditions/H2DatabaseCondition.java
+++
@@ -1,9 +0,0 @@
-package com.epam.mentoring.data.config.conditions;
-
-public class H2DatabaseCondition  {
-//
-//	@Override
-//	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-//		return context.getEnvironment().getProperty("app.database.type").equalsIgnoreCase("H2");
-//	}
-}
--- backend/dao/src/main/resources/delete_tables.sql
+++
@@ -1,8 +0,0 @@
-SET REFERENTIAL_INTEGRITY FALSE;
-DROP TABLE IF EXISTS ${product.table.name};
-DROP TABLE IF EXISTS ${product_type.table.name};
-DROP TABLE IF EXISTS ${product_income.table.name};
-DROP TABLE IF EXISTS ${product_outcome.table.name};
-DROP TABLE IF EXISTS ${supplier.table.name};
-DROP TABLE IF EXISTS ${user.table.name};
-SET REFERENTIAL_INTEGRITY TRUE;
